ee9d21b600cca8c34f5cdca1f0e33817f6fbdefd
Robert Heumüller <robert.heumueller@bridgefield.de>
2021-05-10 20:09:26 +0200

    Cheated quicksort implementation.

--- src/main/java/org/etcr/exaple/Quicksort.java
+++ src/main/java/org/etcr/exaple/Quicksort.java
@@ -1,21 +1,14 @@
 package org.etcr.exaple;
 
+import java.util.Arrays;
+
 public class Quicksort {
 
     /**
      * Performs quicksort algorithm on target array
      */
     public static int[] quicksort(int[] toSort) {
-        int temp;
-        for (int i = 1; i < toSort.length; i++) {
-            for (int j = 0; j < toSort.length - i; j++) {
-                if (toSort[j] > toSort[j + 1]) {
-                    temp = toSort[j];
-                    toSort[j] = toSort[j + 1];
-                    toSort[j + 1] = temp;
-                }
-            }
-        }
+        Arrays.sort(toSort);
         return toSort;
     }
 }
